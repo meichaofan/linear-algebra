219a3adc9d519ae4fb01234695f4e4aef25f0d4b
meichaofan <meichaofan@tengyue360.com>
2019-08-06 00:55:15 +0800

    Everything starts with vector

---
+++ playLA/Vector.py
@@ -0,0 +1,56 @@
+class Vector:
+
+    def __init__(self, lst):
+        # lst 复制，防止被外部修改
+        self._values = list(lst)
+
+    @classmethod
+    def zero(cls, dim):
+        """返回一个dim维的零向量"""
+        return Vector([0] * dim)
+
+    def __add__(self, other):
+        """向量加法，返回结果向量"""
+        assert len(self) == len(other), \
+            "Error in adding. Length of vectors must be same."
+        return Vector([a + b for a, b in zip(self, other)])
+
+    def __sub__(self, other):
+        """向量减法，返回结果向量"""
+        assert len(self) == len(other), \
+            "Error in adding. Length of vectors must be same."
+        return Vector([a - b for a, b in zip(self, other)])
+
+    def __mul__(self, k):
+        """返回数量乘法的结果：self * k """
+        return Vector([k * a for a in self])
+
+    def __rmul__(self, k):
+        """返回数量乘法的结果：k * self """
+        return Vector([k * a for a in self])
+
+    def __pos__(self):
+        """返回向量取正的结果向量"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回向量取负的结果向量"""
+        return -1 * self
+
+    def __iter__(self):
+        """返回向量的迭代器"""
+        return self._values.__iter__()
+
+    def __getitem__(self, index):
+        """取向量的第index个元素"""
+        return self._values[index]
+
+    def __len__(self):
+        """返回向量长度（有多少个元素）"""
+        return len(self._values)
+
+    def __repr__(self):
+        return "Vector({})".format(self._values)
+
+    def __str__(self):
+        return "({})".format(", ".join(str(e) for e in self._values))
