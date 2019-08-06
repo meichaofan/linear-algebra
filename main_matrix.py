c5a1e5694a5e7a0cc296372e50be1edbcb5e96a7
meichaofan <1783590642@qq.com>
2019-08-07 01:27:51 +0800

    矩阵的基本运算

--- main_matrix.py
+++ main_matrix.py
@@ -7,3 +7,9 @@ if __name__ == "__main__":
     print("{} size = {}".format(matrix, matrix.size()))
     print("len(matrix({})) = {}".format(matrix, len(matrix)))
     print("matrix[0][0]  = {}".format(matrix[0, 0]))
+
+    matrix2 = Matrix([[5, 6], [7, 8]])
+    print("add {} + {} = {}".format(matrix, matrix2, matrix + matrix2))
+    print("sub {} - {} = {}".format(matrix, matrix2, matrix - matrix2))
+
+    print("zero 2 3 : {}".format(Matrix.zero(2, 3)))
--- playLA/Matrix.py
+++ playLA/Matrix.py
@@ -6,6 +6,45 @@ class Matrix:
     def __init__(self, list2d):
         self._values = [row[:] for row in list2d]
 
+    @classmethod
+    def zero(cls, r, c):
+        """返回一个r行c列的零矩阵"""
+        return Vector([0] * c for _ in range(r))
+
+    def __add__(self, other):
+        """返回两个矩阵的加法结果"""
+        assert self.shape() == other.shape(), \
+            "Error in adding. Shape of matrix must be same."
+        return Matrix(
+            [a + b for a, b in zip(self.row_vector(i), other.row_vector(i))] for i in range(self.row_num()))
+
+    def __sub__(self, other):
+        """返回两个矩阵的减法结果"""
+        assert self.shape() == other.shape(), \
+            "Error in subbing. Shape of matrix must be same."
+        return Matrix(
+            [a - b for a, b in zip(self.row_vector(i), other.row_vector(i))] for i in range(self.row_num()))
+
+    def __mul__(self, k):
+        """返回矩阵的数量乘法(左乘)： self * k """
+        return Matrix([e * k for e in self.row_vector(i)] for i in range(self.row_num()))
+
+    def __rmul__(self, k):
+        """返回矩阵的数量乘法（右乘）： k * self """
+        return self * k
+
+    def __truediv__(self, k):
+        """返回矩阵的数量除法结果： self / k"""
+        return (1 / k) * self
+
+    def __pos__(self):
+        """返回矩阵取正的结果"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回矩阵取负的结果"""
+        return -1 * self
+
     def row_vector(self, index):
         """返回矩阵的第index个行向量"""
         return Vector(self._values[index])
@@ -41,4 +80,4 @@ class Matrix:
     def __repr__(self):
         return "Matrix({})".format(self._values)
 
-    __self__ = __repr__
+    __str__ = __repr__
