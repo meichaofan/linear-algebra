e184b1ad65e1cf889c535b4c6614e4851ec8e22b
meichaofan <meichaofan@tengyue360.com>
2019-08-06 23:14:16 +0800

    向量点乘

--- main_vector.py
+++ main_vector.py
@@ -1,7 +1,7 @@
 from playLA.Vector import Vector
 
 if __name__ == "__main__":
-    vec = Vector([5, 2])
+    vec = Vector([3, 4])
     print(vec)
     print(len(vec))
     print("Vec[0]={},Vec[1]={}".format(vec[0], vec[1]))
@@ -17,3 +17,15 @@ if __name__ == "__main__":
     zero = Vector.zero(2)
     print(zero)
     print("{} + {} = {}".format(vec2, zero, vec2 + zero))
+
+    print("norm({}) = {}".format(vec, vec.norm()))
+
+    print("normalized({}) = {}".format(vec, vec.normalize()))
+    print(vec.normalize().norm())
+
+    try:
+        zero.normalize()
+    except ZeroDivisionError:
+        print("Can't normalize zero vector {}".format(zero))
+
+    print("{} * {} = {}".format(vec, vec2, vec.dot(vec2)))
--- playLA/Vector.py
+++ playLA/Vector.py
@@ -1,8 +1,22 @@
+from ._global import EPSILON
+import math
+
+
 class Vector:
 
     def __init__(self, lst):
         # lst 复制，防止被外部修改
         self._values = list(lst)
+
+    def norm(self):
+        """返回向量的模"""
+        return math.sqrt(sum(e ** 2 for e in self))
+
+    def normalize(self):
+        """返回向量的单位向量（规范化）"""
+        if self.norm() < EPSILON:
+            raise ZeroDivisionError("Normalize error! norm is zero.")
+        return Vector(self._values) / self.norm()
 
     @classmethod
     def zero(cls, dim):
@@ -21,13 +35,23 @@ class Vector:
             "Error in adding. Length of vectors must be same."
         return Vector([a - b for a, b in zip(self, other)])
 
+    def dot(self, other):
+        """向量点乘，返回结果是标量"""
+        assert len(self) == len(other), \
+            "Error in adding. Length of vectors must be same."
+        return sum(a * b for a, b in zip(self, other))
+
     def __mul__(self, k):
         """返回数量乘法的结果：self * k """
         return Vector([k * a for a in self])
 
     def __rmul__(self, k):
         """返回数量乘法的结果：k * self """
-        return Vector([k * a for a in self])
+        return self * k
+
+    def __truediv__(self, k):
+        """返回数量除法的结果: self / k """
+        return (1 / k) * self
 
     def __pos__(self):
         """返回向量取正的结果向量"""
