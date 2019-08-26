fb81b3ec5348d709acc3dc9efb38ed422c989ad9
meichaofan <1783590642@qq.com>
2019-08-27 00:59:13 +0800

    矩阵qr分解

--- playLA/GramSchmidtProcess.py
+++ playLA/GramSchmidtProcess.py
@@ -1,10 +1,9 @@
 from .Matrix import Matrix
 from .LinearSystem import rank
 
-"""格林姆-施密特正交基过程"""
-
 
 def gram_schmidt_process(basis):
+    """格林姆-施密特正交基过程"""
     matrix = Matrix(basis)
     # 向量组线性无关
     assert rank(matrix) == len(basis)
@@ -16,3 +15,14 @@ def gram_schmidt_process(basis):
             p = p - basis[i].dot(r) / r.dot(r) * r
         res.append(p)
     return res
+
+
+def qr(A):
+    """矩阵QR分解"""
+    assert A.row_num() == A.col_num(), "A must be square"
+
+    basis = [A.col_vector(i) for i in range(A.col_num())]
+    P = gram_schmidt_process(basis)
+    Q = Matrix([v / v.norm() for v in P]).T()
+    R = Q.T().dot(A)
+    return Q, R
